04c1e2bb3884244628b35dfcb2a7fbf931f0238e
brandonqr <brandonqr1@gmail.com>
2020-03-01 11:44:43 +0100

    Updates

---
+++ precache-manifest.4add048db57a2c22e8ade4876ee8f679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebb00350d128a835861c201e4fde4d7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "53f4e2ce29308c0c2932",
+    "url": "/monster-rolodex/static/css/main.6e445e6a.chunk.css"
+  },
+  {
+    "revision": "8c39acc1117c8474bddd",
+    "url": "/monster-rolodex/static/js/2.f035c78a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.f035c78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f4e2ce29308c0c2932",
+    "url": "/monster-rolodex/static/js/main.e787fb2b.chunk.js"
+  },
+  {
+    "revision": "ba1e1cd36c92d8465f27",
+    "url": "/monster-rolodex/static/js/runtime-main.5b741a2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
