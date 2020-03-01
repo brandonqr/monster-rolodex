217e25fa7066edcb172e21572f1631a85c4db5a5
brandonqr <brandonqr1@gmail.com>
2020-03-01 11:46:51 +0100

    adding gh page

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.component";
+import { CardList } from "./components/card-list/card-list.component";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder={"Search monsters"}
+          handleChange={this.handleChange}
+        />
+        {/* <CardList monsters={filteredMonsters}/> */}
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
